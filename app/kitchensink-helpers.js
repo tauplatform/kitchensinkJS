dac2c25a8fdedf3cadfbd81efe9856f65669c8e0
vmusulainen <vmusulainen@gmail.com>
2016-11-14 15:43:05 +0300

    The method Kitchensink.resultJsonOutput is introduced

---
+++ app/kitchensink-helpers.js
@@ -0,0 +1,34 @@
+Rho.Kitchensink = Rho.Kitchensink || {};
+
+Rho.Kitchensink.doWithCaution = function (aFunction) {
+    $(".result-area > .alert")
+        .addClass("hidden")
+        .empty();
+    try {
+        aFunction();
+    }
+    catch (e) {
+        Rho.Kitchensink.errorOutput(JSON.stringify(e));
+    }
+};
+
+
+Rho.Kitchensink.resultOutput = function (aString) {
+    $("#result").removeClass("hidden")
+        .append(aString)
+        .append("<br/>");
+};
+
+Rho.Kitchensink.resultJsonOutput = function (anObject) {
+    $("#result").removeClass("hidden")
+        .append("<pre>" + JSON.stringify(anObject, null, 4) + "</pre>")
+        .append("<br/>");
+};
+
+
+
+Rho.Kitchensink.errorOutput = function (aString) {
+    $("#error").removeClass("hidden")
+        .append(aString)
+        .append("<br/>");
+};
